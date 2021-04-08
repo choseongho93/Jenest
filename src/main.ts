655216cdb6d6104869b7a95ff206f1c8cafb2cf5
Nate <choseongho93@naver.com>
2021-04-08 09:21:01 +0900

    gitignore 추가 및 코드 수정

--- src/main.ts
+++ src/main.ts
@@ -4,5 +4,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
+
+  //80포트를 열어주기 위한 작엄 -- 도커 진행예정
+  //await app.listen(80);
+
 }
 bootstrap();
